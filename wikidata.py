f4ea02ff9dfe5784875d996b3f4f34da542c172d
Eduardo Febres <eduardofebresm@gmail.com>
2025-12-09 13:12:06 -0500

    successfully retrieved only authors from Wikidata, using the viaf attribute

--- wikidata.py
+++ wikidata.py
@@ -1,14 +1,21 @@
+'''
+This is a minimal query. In the future, more complex queries can be built. 
+'''
+
 # json is a built-in module - no installation needed!
 import json
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 # Your SPARQL query remains the same
 query_string = """
-SELECT DISTINCT ?author ?authorLabel ?date_of_death WHERE {
-  ?author wdt:P31 wd:Q5;
-          wdt:P27 wd:Q717;
-          wdt:P570 ?date_of_death.
+SELECT DISTINCT ?author ?authorLabel ?date_of_death ?viaf WHERE {
+  ?author wdt:P31 wd:Q5;                 # Instance of human
+          wdt:P27 wd:Q717;               # Country: Venezuela
+          wdt:P570 ?date_of_death;       # Date of death
+          wdt:P214 ?viaf                 # VIAF code
   FILTER(?date_of_death < "1965-01-01"^^xsd:dateTime)
+  
+  
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
 }
 ORDER BY ?date_of_death
